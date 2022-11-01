a94558ab03649b9d6068c699e209fcdadc9d6796
qq66613 <90941043+qq66613@users.noreply.github.com>
2022-11-01 10:50:36 +0700

    Dashboard and HTML CSS and JS
    Dashboard and HTML CSS and JS

---
+++ dashboard/dashboard.js
@@ -0,0 +1,21 @@
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const themeToggle = document.querySelector(".theme-toggle");
+
+menuBtn.addEventListener('click',  () => {
+        sideMenu.style.display = 'block';
+    });
+
+closeBtn.addEventListener('click', () => {
+        sideMenu.style.display = 'none';
+    });
+
+// change theme
+
+themeToggle.addEventListener('click',() => {
+    document.body.classList.toggle('dark-theme-variables');
+
+    themeToggle.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggle.querySelector('span:nth-child(2)').classList.toggle('active');
+});
